5cf608331012c6f9ce254ca17e610d94164a21a2
eloisebarrow <eloisebarrow@gmail.com>
2019-10-05 11:39:46 -0400

    setup base for all react components

---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <h5>&copy; eloiseressbarrow 2019</h5>
+    </div>
+  )
+}
---
+++ client/src/components/LoginRegisterForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoginRegisterForm() {
+  return (
+    <div>
+      <h1>Login/Register Form</h1>
+    </div>
+  )
+}
