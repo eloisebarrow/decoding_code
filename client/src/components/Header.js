fe27c76b20f7c810c719734e235dc1757d6ee9d9
eloisebarrow <eloisebarrow@gmail.com>
2019-10-06 09:53:20 -0400

    header styles wip color palette

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,25 +8,22 @@ export default function Header() {
     <div className="header-container">
       <Link to="/" className="title"><h1>De/Coding Code</h1></Link>
       <nav className="header-nav">
-        <ul>
-          <li> Topics
-            <ul className="header-topics">
-              <Link to="#"><li>General Programming</li></Link>
-              <Link to="#"><li>HTML</li></Link>
-              <Link to="#"><li>CSS</li></Link>
-              <Link to="#"><li>JavaScript</li></Link>
-              <Link to="#"><li>React</li></Link>
-              <Link to="#"><li>Ruby on Rails</li></Link>
-              <Link to="#"><li>Express.js</li></Link>
-              <Link to="#"><li>Node.js</li></Link>
-              <Link to="#"><li>SQL</li></Link>
-              <Link to="#"><li>Git and Github</li></Link>
-            </ul>
-          </li>
-          <Link to="/login"><li>Login</li></Link>
-          <Link to="/register"><li>Sign Up</li></Link>
-        </ul>
-
+        <Link to="/decks">Topics
+          <ul className="header-topics">
+            <Link to="#"><li>General Programming</li></Link>
+            <Link to="#"><li>HTML</li></Link>
+            <Link to="#"><li>CSS</li></Link>
+            <Link to="#"><li>JavaScript</li></Link>
+            <Link to="#"><li>React</li></Link>
+            <Link to="#"><li>Ruby on Rails</li></Link>
+            <Link to="#"><li>Express.js</li></Link>
+            <Link to="#"><li>Node.js</li></Link>
+            <Link to="#"><li>SQL</li></Link>
+            <Link to="#"><li>Git and Github</li></Link>
+          </ul>
+        </Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Sign Up</Link>
       </nav>
 
 
