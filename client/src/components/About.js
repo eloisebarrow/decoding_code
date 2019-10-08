f7121a3bda832738a4c6db4b03fd92f261d3b5ee
eloisebarrow <eloisebarrow@gmail.com>
2019-10-08 16:53:27 -0400

    mobile home styles complete

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
     <div className="about-container">
       <h2>A Software Engineering study guide for new developers.</h2>
       <h3>Explore our decks or create your own.</h3>
-      <Link to="/decks"><button className="all-decks-button">See All Decks</button></Link>
+      <Link to="/decks"><button className="all-decks-button about-button">See All Decks</button></Link>
     </div>
   )
 }
